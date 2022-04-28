b58165b4c6fc2e5ace1fa522ee2cee72c3db7494
Angel Ham <angelalexham@tamu.edu>
2022-04-28 13:26:49 -0500

    Conclusion of Intern Project with NetEdge VR. Main features are Multiplayer Connection using Photonengine and VR moveable character and interactable objects

---
+++ unity-quest-urp-intern-networking-template/Assets/Scripts/PhotonLobby.cs
@@ -0,0 +1,69 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhotonLobby : MonoBehaviourPunCallbacks
+{
+    public static PhotonLobby lobby;
+
+    public GameObject battleButton;
+    public GameObject cancelButton;
+    private void Awake()
+    {
+        lobby = this; //creates a singleton which lives within the main menu scene
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings(); // connects to Master Photon server.
+    }
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("We are now connected to the " + PhotonNetwork.CloudRegion + " server! Photon master server" );
+        PhotonNetwork.AutomaticallySyncScene = true;
+        battleButton.SetActive(true);
+    }
+    
+    public void OnBattleButtonClicked()
+    {
+        Debug.Log("Battle Button was click");
+        battleButton.SetActive(false);
+        cancelButton.SetActive(true);
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("Tried to join a random game but failed. there must be no open games available");
+        CreateRoom();
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        Debug.Log("Tried to create a new room but failed, there must already be a room with the same name");
+        CreateRoom();
+    }
+
+    void CreateRoom()
+    {
+        Debug.Log("Trying to create a new Room");
+        int randomRoomName = Random.Range(0,10000);
+        RoomOptions roomOps = new RoomOptions() { IsVisible = true, IsOpen = true, MaxPlayers = (byte)MultiplayerSetting.multiplayerSetting.maxPlayers};
+        PhotonNetwork.CreateRoom("Room" + randomRoomName,roomOps);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("We are now in a room");
+    }
+
+    public void OnCancelButtonClicked()
+    {
+        Debug.Log("Cancel Button was clicked");
+        cancelButton.SetActive(false);
+        battleButton.SetActive(true);
+        PhotonNetwork.LeaveRoom();
+    }
+}
